7e1dc788e766e556562f81f1cd9b708c6c51af0b
seunghwan <slflfl12@naver.com>
2020-08-06 22:34:38 +0900

    hash level1 kotlin

---
+++ algorithm/src/main/kotlin/programmers/Hash_Level1_Marathon.kt
@@ -0,0 +1,37 @@
+package programmers
+
+object Hash_Level1_Marathon {
+    fun solution(
+        participant: Array<String>, completion: Array<String>
+    ): String {
+
+        var answer = ""
+
+        var hashMap = hashMapOf<String, Int>()
+
+        participant.forEach {
+            hashMap[it] = hashMap.getOrDefault(it, 0) + 1
+        }
+        completion.forEach { key ->
+            hashMap[key]?.let { value ->
+                hashMap.put(key, value - 1)
+            }
+        }
+
+        hashMap.entries.forEach {
+            if (it.value != 0) {
+                answer = it.key
+            }
+        }
+
+        return answer
+    }
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        println(solution(arrayOf("leo", "kiki", "eden"), arrayOf("eden", "kiki")))
+
+    }
+}
