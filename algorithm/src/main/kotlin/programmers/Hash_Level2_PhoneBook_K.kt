1c13bdef79b53e856d1c72bf05c739b5e6e290cc
seunghwan <slflfl12@naver.com>
2020-08-07 22:09:22 +0900

    hash level2 kotlin

---
+++ algorithm/src/main/kotlin/programmers/Hash_Level2_PhoneBook_K.kt
@@ -0,0 +1,30 @@
+package programmers
+
+object Hash_Level2_PhoneBook_K {
+
+    fun solution(
+        phoneBook: Array<String>
+    ): Boolean {
+
+        var answer = true
+
+        for ((i, value) in phoneBook.withIndex()) {
+            for (j in i + 1 until phoneBook.size) {
+                if (value.startsWith(phoneBook[j])) answer = false
+                if (phoneBook[j].startsWith(value)) answer = false
+            }
+        }
+
+        return answer
+    }
+
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(solution(arrayOf("119", "97674223", "1195524421")))
+        println(solution(arrayOf("123", "456", "789")))
+        println(solution(arrayOf("12", "123", "1235", "567", "88")))
+
+
+    }
+}
