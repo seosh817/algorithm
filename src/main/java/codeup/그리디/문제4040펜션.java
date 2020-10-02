3bc41f8a88443e3786a861bf57e4b231d1a0c3f5
seunghwan <slflfl12@naver.com>
2020-10-02 18:43:44 +0900

    코드업 그리디

---
+++ src/main/java/codeup/그리디/문제4040펜션.java
@@ -0,0 +1,27 @@
+package codeup.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문제4040펜션 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String[] dayRoom = br.readLine().split(" ");
+            int day = Integer.parseInt(dayRoom[0]);
+            int roomNum = Integer.parseInt(dayRoom[1]);
+            int changeCount = 0;
+
+            for (int i = 0; i < day; i++) {
+                String rooms = br.readLine();
+            }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
